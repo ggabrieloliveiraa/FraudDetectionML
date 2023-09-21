75a9b8ef036e992ca8ee5a0da91560495848dc45
Jonathan Douglas <jonathan.tavares@vetta.digital>
2023-09-21 02:05:51 -0300

    alterando a estrutura do projeto para melhorar a organização e geração dos dados de saída (imagens)

---
+++ main.py
@@ -0,0 +1,120 @@
+"""
+Trabalho Prático 1 - Inteligência Artificial
+Ciência da Computação - Pontifícia Universidade Católica de Minas Gerais
+Professor(a): Cristiane Neri
+
+Grupo 1:
+Eric Ferreira
+Gabriel Oliveira
+Helena Ferreira
+Mateus Leal
+Jonathan Douglas
+"""
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn import tree
+from sklearn.metrics import precision_recall_curve
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from yellowbrick.classifier import ClassificationReport, ConfusionMatrix
+
+from utils.utils import cm_analysis
+
+
+def main():
+    # Carregando o conjunto de dados
+    db = pd.read_csv("database/card_transdata.csv")
+    X = db.iloc[:, 0:7].values
+    Y = db["fraud"]
+
+    # Dividindo os dados em conjuntos de treinamento e teste
+    x_treino, x_teste, y_treino, y_teste = train_test_split(
+        X, Y, test_size=0.5, stratify=Y, random_state=23
+    )
+
+    # Treinando o modelo DecisionTreeClassifier
+    modelo = DecisionTreeClassifier(criterion="entropy", max_depth=4)
+    modelo.fit(x_treino, y_treino)
+
+    # Realizando previsões no conjunto de teste
+    previsoes = modelo.predict(x_teste)
+
+    # Calculando as probabilidades da classe positiva (fraude)
+    probs = modelo.predict_proba(x_teste)[:, 1]
+
+    # Calculando a precisão e o recall
+    precision, recall, _ = precision_recall_curve(y_teste, probs)
+
+    # Calculando o F1-score
+    f1_score = 2 * (precision * recall) / (precision + recall)
+
+    # Plotando os gráficos de Precisão, Recall e F1-score
+    plt.figure(figsize=(20, 5))
+    plt.subplot(131)
+    plt.plot(recall, precision, marker=".")
+    plt.xlabel("Recall")
+    plt.ylabel("Precisão")
+    plt.title("Curva de Precisão-Recall")
+
+    plt.subplot(132)
+    plt.plot(recall, f1_score, marker=".")
+    plt.xlabel("Recall")
+    plt.ylabel("F1-score")
+    plt.title("Curva de F1-score vs. Recall")
+
+    plt.subplot(133)
+    plt.plot(precision, f1_score, marker=".")
+    plt.xlabel("Precisão")
+    plt.ylabel("F1-score")
+    plt.title("Curva de F1-score vs. Precisão")
+
+    # Salvar os gráficos em um arquivo
+    plt.savefig("images/graficos.png", format="png")
+
+    fig, axs = plt.subplots(1, 3)
+    fig.delaxes(axs[2])
+
+    # Matriz de Confusão
+    cm = ConfusionMatrix(modelo, is_fitted=True)
+    cm.score(x_teste, y_teste)
+
+    # Salvar a matriz de confusão em um arquivo
+    cm_analysis(
+        y_teste, previsoes, "images/confusion_matrix2.png", ["Não fraude", "Fraude"]
+    )
+
+    previsores = [
+        "distance_from_home",
+        "distance_from_last_transaction",
+        "ratio_to_median_purchase_price",
+        "repeat_retailer",
+        "used_chip",
+        "used_pin_number",
+        "online_order",
+    ]
+
+    # limpa a figura presente no plot
+    plt.clf()
+
+    # Plotando a árvore de decisão
+    tree.plot_tree(
+        modelo,
+        feature_names=previsores,
+        class_names=["Não fraude", "Fraude"],
+        filled=True,
+    )
+
+    # Salvar a árvore de decisão em um arquivo
+    plt.savefig("images/arvore.svg", format="svg")
+
+    # Imprimir as estatísticas de classificação
+    # print("Estatísticas de classificação:")
+    # print(classification_report(y_teste, previsoes))
+    visualizer = ClassificationReport(modelo, is_fitted=True)
+    visualizer.score(x_teste, y_teste)
+    visualizer.show()
+
+
+# Função Principal
+if __name__ == "__main__":
+    main()
